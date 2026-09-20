108b3f451d2780742cfa3efd3b195a71aeb68bb4
saiefadnan <saiefadnan078@gmail.com>
2026-09-20 13:02:44 +0600

    day 2: DTOs, CategoriesController, ProductsController, Async CRUD, Status Codes (201, 204, 400, 404)

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,111 @@
+using InvenTrack.Data;
+using InvenTrack.DTOs;
+using InvenTrack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvenTrack.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
+    {
+        var products = await _context.Products.Include(p => p.Category)
+        .Select(p => new ProductDto
+        {
+            Id = p.Id,
+            Name = p.Name,
+            Price = p.Price,
+            StockQuantity = p.StockQuantity,
+            CategoryId = p.CategoryId,
+            CategoryName = p.Category != null ? p.Category.Name : string.Empty
+        }).ToListAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductDto>> GetProduct(int id)
+    {
+        var product = await _context.Products.Include(p => p.Category).FirstOrDefaultAsync(p => p.Id == id);
+        if (product == null) return NotFound();
+
+        return Ok(new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            StockQuantity = product.StockQuantity,
+            CategoryId = product.CategoryId,
+            CategoryName = product.Category != null ? product.Category.Name : string.Empty
+        });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductDto>> PostProduct(CreateProductDto dto)
+    {
+        var categoryExists = await _context.Categories.AnyAsync(c => c.Id == dto.CategoryId);
+        if (!categoryExists) return BadRequest("Category not found");
+
+        var product = new Product
+        {
+            Name = dto.Name,
+            Price = dto.Price,
+            StockQuantity = dto.StockQuantity,
+            CategoryId = dto.CategoryId
+        };
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        var resultDto = new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            StockQuantity = product.StockQuantity,
+            CategoryId = product.CategoryId,
+            CategoryName = product.Category != null ? product.Category.Name : string.Empty
+        };
+        return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, resultDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(int id, CreateProductDto dto)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+
+        var categoryExists = await _context.Categories.AnyAsync(c => c.Id == dto.CategoryId);
+        if (!categoryExists) return BadRequest("Category not found");
+
+        product.Name = dto.Name ?? product.Name;
+        product.Price = dto.Price;
+        product.StockQuantity = dto.StockQuantity ;
+        product.CategoryId = dto.CategoryId ;
+
+        await _context.SaveChangesAsync();
+        return NoContent();
+
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
